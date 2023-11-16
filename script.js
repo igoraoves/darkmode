841a20736f8aa21b4a95296eee9fbfcc9d250089
Igor Alves <90533978+igoraoves@users.noreply.github.com>
2023-11-16 18:11:56 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function darkMode() {
+  const pagina = document.body;
+
+  pagina.classList.toggle("dark-mode");
+
+  const botao = document.querySelector("button");
+
+  const isDarkMode = document.body.classList.contains("dark-mode");
+
+  if (isDarkMode == true) {
+    botao.textContent = "Modo Claro"
+  } else {
+    botao.textContent = "Modo Escuro"
+  }  
+
+}
